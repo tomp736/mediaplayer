0d050b4a7738275b01b13af8fde72950ca376f61
Thomas Pisula <tpisula@labrats.work>
2023-01-09 23:59:16 +0100

    adding prometheus

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import os
 import logging
 
 from routes import base, video, thumb, extensions
+from prometheus_client import start_http_server, Summary
 
 app = Flask(__name__)
 app.register_blueprint(extensions.app_def)
@@ -13,6 +14,7 @@ app.register_blueprint(base.app_base)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
+    start_http_server(8000)
     app.run(
         host=os.environ.get('FLASK_RUN_HOST', '0.0.0.0'), 
         port=int(os.environ.get('FLASK_RUN_PORT', '0')), 
