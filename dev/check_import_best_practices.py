47ae112a3a6c19051ead56c77daf177f70a45010
Thomas Pisula <tpisula@labrats.work>
2023-01-31 12:03:32 +0100

    dev build

--- dev/check_import_best_practices.py
+++ dev/check_import_best_practices.py
@@ -1,43 +1,56 @@
 import ast
 import os
+import sys
+from typing import List
 
-
-def check_import_best_practices(node):
+def check_import_best_practices(node) -> List[str]:
+    messages = []
     if isinstance(node, ast.Import):
         # Check for multiple imports in the same line
         if len(node.names) > 1:
-            return "Multiple imports in the same line are not recommended"
+            messages.append("Multiple imports in the same line are not recommended")
 
     elif isinstance(node, ast.ImportFrom):
         # Check for wildcard imports
         if node.names[0].name == '*':
-            return "Wildcard imports are not recommended"
+            messages.append( "Wildcard imports are not recommended")
         
         # Check for multiple imports in the same line
         if len(node.names) > 1:
-            return "Multiple imports in the same line are not recommended"
+            messages.append( "Multiple imports in the same line are not recommended")
         
         # Check for relative imports
         if node.level > 0:
-            return "Relative imports are not recommended"
+            messages.append( "Relative imports are not recommended")
+    return messages
 
-    return None
-
-def parse_file(file_path):
+def parse_file(file_path) -> List[str]:
+    messages = []
     with open(file_path, 'r') as f:
         code = f.read()
         tree = ast.parse(code)
         for node in ast.walk(tree):
-            result = check_import_best_practices(node)
-            if result:
-                print(f"{file_path}: {result}")
+            node_messages = check_import_best_practices(node)
+            for message in node_messages:                
+                messages.append(f"{file_path}: {message}")
+        
+    return messages
 
 def parse_directory(directory):
+    messages = []
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".py"):
                 file_path = os.path.join(root, file)
-                parse_file(file_path)
+                file_messages = parse_file(file_path)
+                for message in file_messages:                
+                    messages.append(message)
+        
+    return messages
 
 if __name__ == "__main__":
-    parse_directory("../src")
+    directory_messages = parse_directory(".")
+    if directory_messages and len(directory_messages) > 0:
+        for message in directory_messages:                
+            print(message)
+        sys.exit(100)
--- src/pymp_common/providers/MediaProviderRemote.py
+++ src/pymp_common/providers/MediaProviderRemote.py
@@ -3,7 +3,7 @@ import traceback
 import requests
 from typing import IO
 from typing import Dict
-from typing import List
+from typing import List, Deque
 from typing import Union
 
 from pymp_common.dataaccess.http_request_factory import media_request_factory
