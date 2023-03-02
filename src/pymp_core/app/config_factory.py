1c2d898d8a96fd294fd1abf0f26d9451132a18b1
Thomas Pisula <tpisula@labrats.work>
2023-03-02 12:57:48 +0100

    refactoring

--- src/pymp_core/app/config_factory.py
+++ src/pymp_core/app/config_factory.py
@@ -14,7 +14,9 @@ class ConfigFactory:
         self.json_config_reader = json_config_reader
         self.environment_config_reader = environment_config_reader
         self.runtime_config_provider = runtime_config_provider
+        self.load_environment_configs()
 
+    def load_environment_configs(self):
         self.runtime_config_provider.load_config(
             **self.environment_config_reader.load_config(ServerConfig).__dict__)
         self.runtime_config_provider.load_config(
@@ -25,16 +27,16 @@ class ConfigFactory:
             **self.environment_config_reader.load_config(MediaConfig).__dict__)
 
     def create_server_config(self) -> ServerConfig:
-        return cast(ServerConfig, self.runtime_config_provider.get_config(ServerConfig))
+        return self.runtime_config_provider.create_server_config()
 
     def create_flask_config(self) -> FlaskConfig:
-        return cast(FlaskConfig, self.runtime_config_provider.get_config(FlaskConfig))
+        return self.runtime_config_provider.create_flask_config()
 
     def create_redis_config(self) -> RedisConfig:
-        return cast(RedisConfig, self.runtime_config_provider.get_config(RedisConfig))
+        return self.runtime_config_provider.create_redis_config()
 
     def create_media_config(self) -> MediaConfig:
-        return cast(MediaConfig, self.runtime_config_provider.get_config(MediaConfig))
+        return self.runtime_config_provider.create_media_config()
 
     def create_service_configs(self) -> List[ServiceConfig]:
         return cast(List[ServiceConfig], self.json_config_reader.load_config())
