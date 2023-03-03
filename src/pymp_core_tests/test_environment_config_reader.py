76da408efe1bf3389c121072349c0c7bb64443a8
Thomas Pisula <tpisula@labrats.work>
2023-03-03 10:40:01 +0100

    tests

--- src/pymp_core_tests/test_environment_config_reader.py
+++ src/pymp_core_tests/test_environment_config_reader.py
@@ -1,7 +1,6 @@
 import os
 import unittest
 
-from pymp_core.app.config import FlaskConfig, MediaConfig, PympServerRoles, RedisConfig, ServerConfig
 from pymp_core.app.config_source import EnvironmentConfigSource
 
 
@@ -13,18 +12,6 @@ class EnvironmentConfigSourceTest(unittest.TestCase):
         os.environ['SERVER_PROTO'] = 'https'
         os.environ['SERVER_PORT'] = '8080'
 
-        os.environ['FLASK_HOST'] = '0.0.0.0'
-        os.environ['FLASK_PORT'] = '80'
-        os.environ['FLASK_CORS_HEADERS'] = '*'
-
-        os.environ['REDIS_HOST'] = 'test_redis'
-        os.environ['REDIS_PORT'] = '6379'
-
-        os.environ['MEDIA_PATH'] = '/test/media'
-        os.environ['INDEX_PATH'] = '/test/index'
-        os.environ['MEDIA_CHUNK_SIZE'] = '2048'
-        os.environ['THUMB_CHUNK_SIZE'] = '1024'
-
     def tearDown(self):
         del os.environ['SERVER_ID']
         del os.environ['SERVER_ROLES']
@@ -32,24 +19,12 @@ class EnvironmentConfigSourceTest(unittest.TestCase):
         del os.environ['SERVER_PROTO']
         del os.environ['SERVER_PORT']
 
-        del os.environ['FLASK_HOST']
-        del os.environ['FLASK_PORT']
-        del os.environ['FLASK_CORS_HEADERS']
-
-        del os.environ['REDIS_HOST']
-        del os.environ['REDIS_PORT']
-
-        del os.environ['MEDIA_PATH']
-        del os.environ['INDEX_PATH']
-        del os.environ['MEDIA_CHUNK_SIZE']
-        del os.environ['THUMB_CHUNK_SIZE']
-
     def test_load_server_config(self):
         reader = EnvironmentConfigSource()
         config = reader.get_values()
 
         self.assertEqual(config['SERVER_ID'], 'test_server')
-        self.assertEqual(config['SERVER_ROLES'], str(int(PympServerRoles.MEDIA_API | PympServerRoles.META_API | PympServerRoles.THUMB_API)))
+        self.assertEqual(config['SERVER_ROLES'], '14')
         self.assertEqual(config['SERVER_HOST'], 'test_host')
         self.assertEqual(config['SERVER_PROTO'], 'https')
         self.assertEqual(config['SERVER_PORT'], '8080')
